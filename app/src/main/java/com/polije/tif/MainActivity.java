5ce7838078a2f70d4e7a6b632d90d5a2b14cbb63
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-09-11 08:39:50 +0700

    Add Action Bar(Kurang Menarik Tampilan masih default)

--- app/src/main/java/com/polije/tif/MainActivity.java
+++ app/src/main/java/com/polije/tif/MainActivity.java
@@ -1,5 +1,6 @@
 package com.polije.tif;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -22,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Data Mahasiswa");
+        }
 
         addData();
 
